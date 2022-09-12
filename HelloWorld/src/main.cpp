8fc759720ca5fabfbc65572bcc73fb092270a2aa
Kavy <kavyrakulan@gmail.com>
2022-09-12 17:10:10 -0400

    IoT-Lab1
    printing student id, name, flash id and chip id

---
+++ HelloWorld/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+  //start the USB serial monitor port
+  Serial.begin(115200);
+
+  //print some text to the USB port
+  Serial.println("\n\nRhagavy Rakulan(000802106)");
+  Serial.println("Chip ID:"+String(ESP.getChipId()));
+  Serial.println("Flash ID:" +String(ESP.getFlashChipId()));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
